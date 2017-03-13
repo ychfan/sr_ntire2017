f7796a3cc806eab9cbff54c447ccc38b9c877988
Yuchen Fan <fyc0624@gmail.com>
2017-03-13 16:42:39 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-#!bin/bash
+#!/bin/bash
 
 # Specify the name for your job name, this is the job name by which grid engine will 
 # refer to your job, this could be different from name of your executable or name of your script file
@@ -18,27 +18,31 @@
 #$ -l gpu=1
 #
 # Specify the hostname of the machine to run
-#$ -l h="*1|*2"
+#$ -l h="*1|*2|*3"
+
+set -x
 
 EXPR_NAME="try"
-TRAIN_DIR="tmp/"
+TRAIN_DIR="tmp"
 MODEL_NAME="model_res_pre_act"
+DATA_NAME="data_naive_bc"
+HR_FLIST="flist/hr.flist"
+LR_FLIST="flist/lrX2.flist"
 
+source /data/jl_1/shared/ifp/envs/ifp-tf-master/bin/activate
 export CUDA_VISIBLE_DEVICES=`echo $SGE_HGR_gpu | sed 's/GPU//g' | awk -F ' ' '{for(i=1;i<NF;++i)printf "%i,",$i-1; printf "%i",$NF-1}'`
 #export CUDA_VISIBLE_DEVICES=0
 
-MODEL_FILE="$TRAIN_DIR/$MODEL_NAME_$EXPR_NAME"
-# learning rate 0.01 with adam
-python train.py --model_name=$MODEL_NAME --model_file=$MODEL_FILE --learning_rate=0.01
-
+MODEL_FILE="$TRAIN_DIR/$MODEL_NAME-$DATA_NAME-$EXPR_NAME"
+ARGS="--data_name=$DATA_NAME --hr_flist=$HR_FLIST --lr_flist=$LR_FLIST --model_name=$MODEL_NAME --model_file=$MODEL_FILE"
 # learning rate 0.001 with adam
 for i in `seq 1 10`;
 do
-    python train.py --model_name=$MODEL_NAME --model_file=$MODEL_FILE --learning_rate=0.001
+    python train.py $ARGS --learning_rate=0.001
 done
 
 # learning rate 0.0001 with adam
 for i in `seq 1 2`;
 do
-    python train.py --model_name=$MODEL_NAME --model_file=$MODEL_FILE --learning_rate=0.0001
+    python train.py $ARGS --learning_rate=0.0001
 done
