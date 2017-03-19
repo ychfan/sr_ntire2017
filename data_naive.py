a0a14171acefd22b2ad95a285ef1e1d8d7bfa913
Yuchen Fan <yuchenf4@illinois.edu>
2017-03-19 01:48:47 -0500

    add 8x rotation and flip

--- data_naive.py
+++ data_naive.py
@@ -13,9 +13,23 @@ def dataset(hr_flist, lr_flist, resize_func):
     lr_image = tf.image.decode_image(lr_image_file, channels=3)
     hr_image = tf.image.convert_image_dtype(hr_image, tf.float32)
     lr_image = tf.image.convert_image_dtype(lr_image, tf.float32)
-    hr_image = tf.stack([hr_image, tf.image.flip_left_right(hr_image)])
-    lr_image = tf.stack([lr_image, tf.image.flip_left_right(lr_image)])
+    hr_patches0, lr_patches0 = make_patches(hr_image, lr_image, resize_func)
+    hr_patches1, lr_patches1 = make_patches(tf.image.rot90(hr_image), tf.image.rot90(lr_image), resize_func)
+    return tf.concat([hr_patches0, hr_patches1], 0), tf.concat([lr_patches0, lr_patches1], 0)
+
+def make_patches(hr_image, lr_image, resize_func):
+    hr_image = tf.stack(flip([hr_image]))
+    lr_image = tf.stack(flip([lr_image]))
     lr_image = resize_func(lr_image, tf.shape(hr_image)[1:3])
     hr_patches = data.image_to_patches(hr_image)
     lr_patches = data.image_to_patches(lr_image)
-    return hr_patches, lr_patches
+    return hr_patches, lr_patches 
+
+def flip(img_list):
+    flipped_list = []
+    for img in img_list:
+        flipped_list.append(img)
+        flipped_list.append(tf.image.flip_left_right(img))
+        flipped_list.append(tf.image.flip_up_down(img))
+        flipped_list.append(tf.image.rot90(img, k=2))
+    return flipped_list
