96f280633ae67ff3088be188d474cfa7b8f83a25
Yuchen Fan <yuchenf4@illinois.edu>
2017-03-27 15:55:01 -0500

    remove useless models

--- model_conv.py
+++ model_conv.py
@@ -1,8 +1,8 @@
 import tensorflow as tf
 
 def build_model(x, reuse):
-    x = tf.layers.conv2d(x, 64, 3, padding='same', activation=tf.sigmoid, name='conv1')
-    x = tf.layers.conv2d(x, 64, 3, padding='same', activation=tf.sigmoid, name='conv2')
-    x = tf.layers.conv2d(x, 64, 3, padding='same', activation=tf.sigmoid, name='conv3')
-    x = tf.layers.conv2d(x, 1, 1, padding='same', activation=None, name='conv4')
+    x = tf.layers.conv2d(x, 64, 3, activation=tf.sigmoid, name='conv1')
+    x = tf.layers.conv2d(x, 64, 3, activation=tf.sigmoid, name='conv2')
+    x = tf.layers.conv2d(x, 64, 3, activation=tf.sigmoid, name='conv3')
+    x = tf.layers.conv2d(x, 3, 1, activation=None, name='out')
     return x
--- model_res.py
+++ model_res.py
@@ -2,20 +2,25 @@ import tensorflow as tf
 
 def build_model(x, reuse):
     hidden_size = 128
-    projection_size = 32
-    x = conv(x, hidden_size, projection_size, 'conv00', reuse)
+    bottleneck_size = 32
+    x = tf.layers.conv2d(x, hidden_size, 9, activation=None, name='in', reuse=reuse)
     for i in range(10):
-        x = crop_by_pixel(x, 1) + conv(x, hidden_size, projection_size, 'conv'+str(i), reuse)
+        x = crop_by_pixel(x, 1) + conv(x, hidden_size, bottleneck_size, 'conv'+str(i), reuse)
     x = tf.layers.conv2d(x, 3, 1, activation=None, name='out', reuse=reuse)
     return x
 
-def conv(x, hidden_size, projection_size, name, reuse):
+def conv(x, hidden_size, bottleneck_size, name, reuse):
     x = tf.layers.batch_normalization(x)
     x = tf.nn.relu(x)
-    x = tf.layers.conv2d(x, hidden_size, 3, activation=None, name=name+'_filt', reuse=reuse)
+    x = tf.layers.conv2d(x, bottleneck_size, 1, activation=None, name=name+'_proj', reuse=reuse)
+    
     x = tf.layers.batch_normalization(x)
     x = tf.nn.relu(x)
-    x = tf.layers.conv2d(x, projection_size, 1, activation=None, name=name+'_proj', reuse=reuse)
+    x = tf.layers.conv2d(x, bottleneck_size, 3, activation=None, name=name+'_filt', reuse=reuse)
+    
+    x = tf.layers.batch_normalization(x)
+    x = tf.nn.relu(x)
+    x = tf.layers.conv2d(x, hidden_size, 1, activation=None, name=name+'_recv', reuse=reuse)
     return x
 
 def crop_by_pixel(x, num):
