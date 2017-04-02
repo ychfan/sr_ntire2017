7ab3e02ed80a8987778636303094c1b12b510ab3
Yuchen Fan <yuchenf4@illinois.edu>
2017-04-02 00:36:47 -0500

    update gated cnn

--- model_pixelcnn.py
+++ model_pixelcnn.py
@@ -5,16 +5,20 @@ upsample = False
 
 def build_model(x, scale, training, reuse):
     hidden_size = 128
-    projection_size = 32
-    x = conv_gated(x, hidden_size, projection_size, 'conv00', reuse)
+    bottleneck_size = 32
+    x = tf.layers.conv2d(x, hidden_size, 3, activation=None, name='in', reuse=reuse)
     for i in range(10):
-        x = util.crop_by_pixel(x, 1) + conv_gated(x, hidden_size, projection_size, 'conv'+str(i), reuse)
+        x = util.crop_by_pixel(x, 1) + conv_gated(x, hidden_size, bottleneck_size, training, 'conv'+str(i), reuse)
+    x = tf.nn.relu(x)
     x = tf.layers.conv2d(x, 3, 1, activation=None, name='out', reuse=reuse)
     return x
 
-def conv_gated(x, hidden_size, projection_size, name, reuse):
-    x_filter = tf.layers.conv2d(x, hidden_size, 3, activation=tf.tanh, name=name+'_filt', reuse=reuse)
-    x_gate = tf.layers.conv2d(x, hidden_size, 3, activation=tf.sigmoid, name=name+'_gate', reuse=reuse)
+def conv_gated(x, hidden_size, bottleneck_size, training, name, reuse):
+    x = tf.nn.relu(x)
+    x = tf.layers.conv2d(x, bottleneck_size, 1, activation=None, name=name+'_proj', reuse=reuse)
+    x = tf.nn.relu(x)
+    x_filter = tf.layers.conv2d(x, bottleneck_size, 3, activation=tf.tanh, name=name+'_filt', reuse=reuse)
+    x_gate = tf.layers.conv2d(x, bottleneck_size, 3, activation=tf.sigmoid, name=name+'_gate', reuse=reuse)
     x = x_filter * x_gate
-    x = tf.layers.conv2d(x, projection_size, 1, activation=None, name=name+'_proj', reuse=reuse)
+    x = tf.layers.conv2d(x, hidden_size, 1, activation=None, name=name+'_recv', reuse=reuse)
     return x
